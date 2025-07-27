a38eea6c7bc82591452b28b985b0d6a47af2ed20
srj1603 <chavansuraj573@gmail.com>
2025-07-27 20:56:00 +0530

    Your message

---
+++ src/main/java/MakeMyTrip/MakeMyTrip/flight.java
@@ -0,0 +1,10 @@
+package MakeMyTrip.MakeMyTrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+    @GetMapping("/flight")
+    public String getData() {return  "Please  book flights indigo ticket with 50% discount" ; }
+}
---
+++ src/test/java/MakeMyTrip/MakeMyTrip/MakeMyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package MakeMyTrip.MakeMyTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMyTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
