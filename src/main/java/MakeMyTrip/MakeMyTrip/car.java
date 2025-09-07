eb9d07eefef541a563cb49fbfa7f6dc9d00b1880
srj1603 <chavansuraj573@gmail.com>
2025-09-07 23:46:34 +0530

    added new linw

--- src/main/java/MakeMyTrip/MakeMyTrip/car.java
+++ src/main/java/MakeMyTrip/MakeMyTrip/car.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class car {
     @GetMapping("/car")
-    public String getData() {return  "Please jwuune  jsj js sjkgit Hotel JW marriot  with 40% discount" ; }
+    public String getData() {return  "Please 111 jwuune  jsj js sjkgit Hotel JW marriot  with 40% discount" ; }
 }
