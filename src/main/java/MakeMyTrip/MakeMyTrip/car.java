7f3ff05343076dd3c5287c24f4ee6f162e679b61
srj1603 <chavansuraj573@gmail.com>
2025-09-07 22:35:38 +0530

    Merge branch 'dev' new loln

---
+++ src/main/java/MakeMyTrip/MakeMyTrip/car.java
@@ -0,0 +1,10 @@
+package MakeMyTrip.MakeMyTrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class car {
+    @GetMapping("/car")
+    public String getData() {return  "Please jwuune  jsj js sjkgit Hotel JW marriot  with 40% discount" ; }
+}
